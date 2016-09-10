8cc065c75fc38792670b3f5b635eff629c6224dd
Erick <github@ericklavoie.com>
2016-09-10 10:16:17 +0200

    Initial commit

---
+++ implementations/matlab/runner.m
@@ -0,0 +1,24 @@
+function runner(size)
+
+tic();
+max_length = 0;
+max_num = 0;
+for i = 1:size
+    length = collatz(i);
+    if length > max_length
+        max_length = length;
+        max_num = i;
+    end
+end
+elapsedTime = toc();
+
+disp('{')
+disp('"options":')
+disp(size)
+disp(', "time":')
+disp(elapsedTime)
+disp(', "output":')
+disp(max_num);
+disp('}')
+
+end
